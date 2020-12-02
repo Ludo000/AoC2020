52800f8b141cbb7b440671388e9d2cd1f47946a2
moi <moi@pop-os.localdomain>
2020-12-02 14:42:46 +0100

    day 1 and 2

---
+++ 2.py
@@ -0,0 +1,33 @@
+from parse import *
+
+def main():
+    # part 1
+     f = open("input2.txt", "r")
+     i = 0
+     for x in f:
+        line = parse("{}-{} {}: {}", x)
+        min = int(line[0])
+        max = int(line[1])
+        c = line[2]
+        psw = line[3]
+        occurence = psw.count(c)
+        if(occurence >= min and occurence <= max): i+=1
+     print("part1 : ", i)
+     f.close() 
+
+    # part 2
+     f = open("input2.txt", "r")
+     i = 0
+     for x in f:
+        line = parse("{}-{} {}: {}", x)
+        psw = line[3]
+        pos1 = int(line[0]) - 1
+        pos2 = int(line[1]) - 1
+        if((psw[pos1] == line[2]) !=  (psw[pos2] == line[2])): i+=1
+     print("part2 : ", i)
+     f.close() 
+     return
+
+
+if __name__ == "__main__":
+    main()
